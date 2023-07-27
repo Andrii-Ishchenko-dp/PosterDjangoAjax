984360cdefca8a98de722e5d94c7c3b5beed466b
Andrii-Ishchenko-dp <76679740+Andrii-Ishchenko-dp@users.noreply.github.com>
2023-07-27 09:08:18 +0300

    Делаю токен доступа невидимым на странице

--- ProjectOrion/DownApp/views.py
+++ ProjectOrion/DownApp/views.py
@@ -187,6 +187,7 @@ def export_data(request):
 
 
         elif request.POST.get('type_of_down') == '2':
+            access_token = request.POST.get('access_tok')
             print('Код доступа при выгрузке остатков: ', access_token)
             storage = []  # склады в заведении, id, название
 
@@ -302,5 +303,5 @@ def download_file(request, filename):
             response['Content-Disposition'] = 'attachment; filename=' + filename
             return response
     else:
-        return HttpResponse('Файл не найден', status=404)
+        return HttpResponse('В указанный период нет данных. Перезайдите в приложение й выберете другой временной промежуток.', status=404)
 
